189ef3054b82a6737e44891e0c6ad31e6eea611d
Claire Fields <clairefields15@gmail.com>
2021-08-10 09:26:06 -0600

    add check for error fn in apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,17 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+  .then(response => checkForError(response))
+}
+
+const checkForError = (response) => {
+  if(response.ok) {
+    return response.json()
+  } else if (response.status === 404) {
+    throw new Error('404')
+  } else if (response.status === 500) {
+    throw new Error('500')
+  } else {
+    throw new Error('Something went wrong')
+  }
+
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {getOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-class App extends Component {
-  constructor(props) {
-    super();
-  }
+export const App = () => {
 
-  componentDidMount() {
+
+  useEffect(() => {
     getOrders()
+      .then(order => console.log(order))
       .catch(err => console.error('Error fetching:', err));
-  }
+  }, [])
 
-  render() {
     return (
       <main className="App">
         <header>
@@ -22,11 +20,9 @@ class App extends Component {
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        {/* <Orders orders={this.state.orders}/> */}
       </main>
     );
-  }
 }
 
 
-export default App;
